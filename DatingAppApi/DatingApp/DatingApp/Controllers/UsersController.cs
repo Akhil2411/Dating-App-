005831aa30484024c7269d9c70d49896b0afbc31
Akhil.Raju <Akhil.Raju@gbg.com>
2022-05-29 16:04:37 +0530

    first commit

---
+++ DatingAppApi/DatingApp/DatingApp/Controllers/UsersController.cs
@@ -0,0 +1,43 @@
+﻿using DatingApp.Data;
+using DatingApp.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.Controllers
+{
+   
+    public class UsersController : BaseApiController
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        //The output will be a list of AppUser
+        //So List<AppUser> can be also used as a return type
+        //But List offers too many options like sort,search etc..which we dont want at the moment..So we use I enumerable instead
+        //bcz we are simply returning a list
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+
+            return await _context.Users.ToListAsync();
+        }
+
+
+        [Authorize]
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUserById(int id)
+        {
+           
+            return await _context.Users.FindAsync(id);
+        }
+    }
+}
